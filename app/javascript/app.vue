25529af025b5381aa1c4260f4d6a4658c07d8f6e
Adnilson <adnilson.delgado@outlook.com>
2021-04-14 22:27:52 +0000

    Add ImageModal, refactor previous code

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <input v-model="searchQuery">
-    <button v-on:click="getImages">Search</button>
-    <button v-show="searchQuery" v-on:click="removeSearchQuery">X</button>
+    <section class="search">
+      <input v-model="searchQuery">
+      <button @click="getImages">Search</button>
+      <button v-show="searchQuery" @click="removeSearchQuery">X</button>
+    </section>
     <ul v-if="images">
       <li v-for="i in images" :key="i.id">
-        <img v-bind:src="'https://live.staticflickr.com/' + i.server + '/' + i.id + '_' + i.secret + '_q.jpg'">
+        <img :src="'https://live.staticflickr.com/' + i.server + '/' + i.id + '_' + i.secret + '_q.jpg'" @click="passToModal(i)">
       </li>
     </ul>
+    <ImageModal v-bind:image="selectedImage" v-if="showModal" @close="showModal = false" />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ImageModal from "./packs/components/ImageModal.vue"
 
 export default {
   data: function () {
     return {
       images: [],
-      searchQuery: ''
+      searchQuery: '',
+      selectedImage: '',
+      showModal: false
     }
   },
   methods: {
@@ -29,21 +35,33 @@ export default {
         .catch(error => console.log(error))
     },
     removeSearchQuery: function () {
-      this.searchQuery = '',
+      this.searchQuery = ''
       this.images = []
+    },
+    passToModal: function (i) {
+      this.selectedImage = i
+      this.showModal = true
     }
+  },
+  components: {
+    ImageModal
   }
 }
 
 </script>
 
 <style scoped>
+#app {
+  display: flex;
+  flex-flow: column;
+}
+
 ul {
-  background-color: black;
   display: flex;
   flex-flow: wrap;
   list-style-type: none;
 }
+
 li {
   margin: 0 0.2em;
 }
